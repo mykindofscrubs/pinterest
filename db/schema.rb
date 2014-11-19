f677c9f21ec036261bb079475f934f0efc886da8
Niger Hall <h.niger@yahoo.com>
2014-11-19 11:55:31 -0500

    added user_id to pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119155043) do
+ActiveRecord::Schema.define(version: 20141119165151) do
 
   create_table "boards", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141119155043) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
